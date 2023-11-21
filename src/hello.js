be182d4ce7da6775a352d9517197d73f117c2a1b
Alexander <alexander.shumenko@gmail.com>
2023-11-21 10:10:03 +0100

    Create hello.js

---
+++ src/hello.js
@@ -0,0 +1,5 @@
+document.addEventListener("DOMContentLoaded", function() {
+    var h1Element = document.createElement("h1");
+    h1Element.textContent = "Hello World!";
+    document.body.appendChild(h1Element);
+});
